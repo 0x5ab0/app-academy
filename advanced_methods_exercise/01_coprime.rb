ec25864b02243e21eb50105389a2448667579282
Josep Coronado <josepcoronado1@gmail.com>
2021-05-05 14:57:19 +0200

    Solve 01. Coprime

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,13 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def get_divisors(num)
+    (2...num).select { |divisor| num % divisor == 0 }
+end
+
+def coprime?(num1, num2)
+    return get_divisors(num1).none? { |n| get_divisors(num2).include?(n) }
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
