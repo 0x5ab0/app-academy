47f44c86a65b74d91d82369ce710a5cf1ba49c76
Josep Coronado <josepcoronado1@gmail.com>
2021-05-06 22:44:42 +0200

    Solve RSpec Exercise 2

---
+++ rspec_exercise_2/lib/part_2.rb
@@ -0,0 +1,31 @@
+def palindrome?(str)
+    rev_str = ''
+
+    i = str.length - 1
+    while i >= 0
+        rev_str += str[i]
+
+        i -= 1
+    end
+
+    return true if str == rev_str
+    false
+end
+
+def substrings(str)
+    subs = []
+
+    str.each_char.with_index do |char1, i|
+        str.each_char.with_index do |char2, j|
+            subs << str[i..j] if i <= j
+        end
+    end
+
+    return subs
+end
+
+def palindrome_substrings(str)
+    subs = substrings(str)
+
+    subs.select { |sub| palindrome?(sub) && sub.length > 1 }
+end
