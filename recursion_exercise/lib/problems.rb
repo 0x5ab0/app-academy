e8e506bc56ded7c4951434d73a10d1220d1ef312
Josep Coronado <josepcoronado1@gmail.com>
2021-05-11 11:52:49 +0200

    Solve Recursion Exercise

--- recursion_exercise/lib/problems.rb
+++ recursion_exercise/lib/problems.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 # Write a method, pow(base, exponent), that takes in two numbers.
 # The method should calculate the base raised to the exponent power.
 # You can assume the exponent is always positive.
@@ -12,7 +14,8 @@
 # pow(3, 4) # => 81
 # pow(4, 3) # => 64
 def pow(base, exponent)
-
+    return 1 if exponent == 0
+    base * pow(base, exponent - 1)
 end
 
 
@@ -35,7 +38,9 @@ end
 # lucas_number(5)   # =>    11
 # lucas_number(9)   # =>    76
 def lucas_number(n)
-
+    return 2 if n == 0
+    return 1 if n == 1
+    lucas_number(n - 1) + lucas_number(n - 2)
 end
 
 
@@ -51,7 +56,8 @@ end
 # sum_array([5, 2])         # => 7
 # sum_array([4, 10, -1, 2]) # => 15
 def sum_array(array)
-
+    return 0 if array.empty?
+    array.pop + sum_array(array)
 end
 
 
@@ -67,7 +73,8 @@ end
 # reverse_string("internet")    # => "tenretni"
 # reverse_string("friends")     # => "sdneirf"
 def reverse_string(str)
-
+    return '' if str.empty?
+    str[-1] + reverse_string(str[0...-1])
 end
 
 
@@ -100,5 +107,13 @@ end
 #     2-dimensional array: [['some data']]
 #     3-dimensional array: [[['some data']]]
 def flatten(data)
+    return [data] if !data.is_a?(Array)
+    
+    flattened = []
+    
+    data.each do |el|
+        flattened += flatten(el)
+    end
 
+    flattened
 end
