ae066fc417f726cb909dba36063733b12b19ed79
josep <josepcoronado1@gmail.com>
2021-05-17 11:03:24 +0200

    Add Board class

---
+++ tic-tac-toe/v1/board.rb
@@ -0,0 +1,70 @@
+require 'byebug'
+
+class Board
+    def initialize
+        @grid = Array.new(3) { Array.new(3, '_') }
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+    end
+
+    def valid?(pos)
+        row, col = pos
+        pos.all? { |i| i >= 0 && i < @grid.length }
+    end
+
+    def empty?(pos)
+        self[pos] == '_'
+    end
+
+    def place_mark(pos, mark)
+        raise 'invalid position' if !valid?(pos) || !empty?(pos)
+        self[pos] = mark
+    end
+
+    def print
+        @grid.each { |row| puts row.join(' ') }
+    end
+
+    def win_row?(mark)
+        @grid.any? { |row| row.all?(mark) }
+    end
+
+    def win_col?(mark)
+        @grid.transpose.any? { |col| col.all?(mark) }
+    end
+
+    def win_diagonal?(mark)
+        d1 = (0...@grid.length).all? do |i|
+            pos = [i, i]
+            self[pos] == mark
+        end
+
+        d2 = (0...@grid.length).all? do |i|
+            pos = [i, @grid.length - 1 - i]
+            self[pos] == mark
+        end
+
+        d1 || d2
+    end
+
+    def win?(mark)
+        win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+    end
+
+    def empty_positions?
+        (0...@grid.length).any? do |row|
+            (0...@grid.length).any? do |col|
+                pos = [row, col]
+                self.empty?(pos)
+            end
+        end
+    end
+end
