bbe75d393e8159869b592d0c1f64c85470642032
Josep <josepcoronado1@gmail.com>
2021-06-21 14:37:19 +0200

    Add checkmate status checker

--- chess/pieces/piece.rb
+++ chess/pieces/piece.rb
@@ -23,4 +23,16 @@ class Piece
         # subclass implements this with unicode chess char
         raise NotImplementedError
     end
+
+    def valid_moves
+        moves.reject { |end_pos| move_into_check?(end_pos) }
+    end
+
+    private
+
+    def move_into_check?(end_pos)
+        test_board = board.dup
+        test_board.move_piece!(pos, end_pos)
+        test_board.in_check?(color)
+    end
 end
