a8371b0ef41a54f79b70babbec12033b6df6dbc6
Josep <josepcoronado1@gmail.com>
2021-06-28 18:18:59 +0200

    Phase 3: Hash Set

--- hashmap-lru-cache/lib/p03_hash_set.rb
+++ hashmap-lru-cache/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :store, :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,24 +7,37 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key.hash] << key
+    self.count += 1
+    resize! if self.count > num_buckets
+
+    key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self.count -= 1 if self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
-    @store.length
+    self.store.length
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) }
   end
 end
