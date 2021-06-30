2af4c28455f93fe2d465333d15644a1095b07b06
Josep <josepcoronado1@gmail.com>
2021-06-30 20:57:32 +0200

    Implement User model class

---
+++ aa-questions/user.rb
@@ -0,0 +1,47 @@
+require_relative 'questions_database'
+require_relative 'question'
+require_relative 'question_follow'
+require_relative 'question_like'
+require_relative 'reply'
+
+class User
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+        data.map { |datum| User.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        user_data = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+            SELECT
+                users.*
+            FROM
+                users
+            WHERE
+                users.id = :id
+        SQL
+
+        user_data.empty? ? nil : User.new(user_data.first)
+    end
+
+    def self.find_by_name(fname, lname)
+        user_data = QuestionsDatabase.instance.execute(<<-SQL, fname: fname, lname: lname)
+            SELECT
+                users.*
+            FROM
+                users
+            WHERE
+                users.fname = :fname AND users.lname = :lname
+        SQL
+
+        user_data.empty? ? nil : user_data.map { |user| User.new(user) }
+    end
+
+    attr_reader :id
+    attr_accessor :fname, :lname
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+end
