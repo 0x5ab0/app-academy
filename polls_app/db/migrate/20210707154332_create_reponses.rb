c2c1cc21444a88219af590b595344728556046f3
Josep <josepcoronado1@gmail.com>
2021-07-07 19:04:22 +0200

    Migrations and Associations

---
+++ polls_app/db/migrate/20210707154332_create_reponses.rb
@@ -0,0 +1,13 @@
+class CreateReponses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reponses do |t|
+      t.integer :respondent_id, null: false
+      t.integer :answer_choice_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :reponses, :respondent_id
+    add_index :reponses, :answer_choice_id
+  end
+end
