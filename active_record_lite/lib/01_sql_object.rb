36d734ec7f25778de1c53d554819f94e5ccbe267
Josep <josepcoronado1@gmail.com>
2021-07-10 18:25:10 +0200

    Columns and attribute reader, finalize method

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -3,10 +3,30 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+      LIMIT
+        0
+    SQL
+
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -34,7 +54,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
