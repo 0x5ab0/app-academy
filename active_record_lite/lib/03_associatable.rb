926d4cdcfbedf6e27b45ebb1b8d05d589ebe2fad
Josep <josepcoronado1@gmail.com>
2021-07-11 15:32:53 +0200

    #belongs_to / #has_many

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -46,13 +46,31 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => key_val)
+        .first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
@@ -61,5 +79,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
