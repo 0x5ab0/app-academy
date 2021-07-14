72f908ab2921791d079791ccd0079837dc1e6b00
Josep <josepcoronado1@gmail.com>
2021-07-14 16:48:20 +0200

    Comments functionality

--- art-share-api/config/routes.rb
+++ art-share-api/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :update, :destroy] do
     resources :artworks, only: :index
+    resources :comments, only: :index
   end
 
-  resources :artworks, only: [:create, :show, :update, :destroy]
+  resources :artworks, only: [:create, :show, :update, :destroy] do
+    resources :comments, only: :index
+  end
+
   resources :artwork_shares, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
 end
