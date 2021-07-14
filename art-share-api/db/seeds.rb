8de26ff956c6ba7c80d7b0b0ae6bfaaffe5776a5
Josep <josepcoronado1@gmail.com>
2021-07-14 12:54:26 +0200

    Seeding

--- art-share-api/db/seeds.rb
+++ art-share-api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Artwork.destroy_all
+ArtworkShare.destroy_all
+
+user1 = User.create!(username: "Satoshi")
+user2 = User.create!(username: "Josep")
+artwork1 = Artwork.create!(title: "Genesis Block", image_url: "google.com", artist_id: user1.id)
+artwork2 = Artwork.create!(title: "Sun and Field", image_url: "pinterest.com", artist_id: user2.id)
+ArtworkShare.create!(artwork_id: artwork1.id, viewer_id: user2.id)
+ArtworkShare.create!(artwork_id: artwork2.id, viewer_id: user1.id)
