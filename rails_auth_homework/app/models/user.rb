5424f6b9f50a6d96a7c084d39ae9f8155157afde
Josep <josepcoronado1@gmail.com>
2021-07-18 15:11:58 +0200

    Add helper methods to the User model

--- rails_auth_homework/app/models/user.rb
+++ rails_auth_homework/app/models/user.rb
@@ -3,5 +3,38 @@ class User < ApplicationRecord
     validates :password_digest, presence: { message: "Password can't be blank" }
     validates :session_token, presence: true, uniqueness: true
     validates :password, length { minimum: 6, allow_nil: true }
-    before_validation :ensure_session_token 
+    before_validation :ensure_session_token
+
+    attr_reader :password
+    
+    def find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && password_match?(user, password)
+        nil
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+    
+    private
+
+    def password_match?(user, password)
+        BCrypt::Password.new(user.password_digest).is_password?(password)
+    end
 end
