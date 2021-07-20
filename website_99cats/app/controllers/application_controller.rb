406e8d0329fe990af1c785c5cc5e7f972916a868
Josep <josepcoronado1@gmail.com>
2021-07-20 19:24:37 +0200

    UsersController and SessionsController

--- website_99cats/app/controllers/application_controller.rb
+++ website_99cats/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    private
+
+    def current_user
+        User.find_by(session_token: session[:session_token])
+    end
+
+    def login_user!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout_user!(user)
+        current_user.reset_session_token!
+        session_token[:session_token] = nil
+    end
 end
