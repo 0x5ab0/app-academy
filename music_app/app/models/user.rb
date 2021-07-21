4abed34c708d6bcc21314af2f1a4aa128406afa3
Josep <josepcoronado1@gmail.com>
2021-07-21 20:22:57 +0200

    User model and migration

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+    validates :email, presence: true
+    validates :password_digest, presence: true, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        
+        user.is_password?(password) && user ? user : nil
+    end
+
+    def self.generate_session_token
+        self.session_token = SecureRandom.urlsafe_base64(16)
+    end
+
+    attr_reader :password
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(@password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+end
