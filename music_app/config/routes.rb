de1a6fd015f0a2b884dbb589a06485549e6e52cd
Josep <josepcoronado1@gmail.com>
2021-07-22 21:11:09 +0200

    Albums

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
-  resources :bands
+
+  resources :users, only: [:show, :new, :create]
+  
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:show, :create, :edit, :update, :destroy]
 end
