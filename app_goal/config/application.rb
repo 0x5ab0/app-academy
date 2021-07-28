d6bc41607243b1277631c404fd125051a0e7e71b
Josep <josepcoronado1@gmail.com>
2021-07-28 12:53:31 +0200

    Finish Rails and RSpec tests

--- app_goal/config/application.rb
+++ app_goal/config/application.rb
@@ -6,25 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AppGoal
+module GoalApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Auto-generate test files
-    config.generators do |g|
-      g.test_framework :rspec,
-        :fixtures => false,
-        :view_specs => false,
-        :helper_specs => false,
-        :routing_specs => false,
-        :controller_specs => true,
-        :request_specs => false
-    end
   end
 end
