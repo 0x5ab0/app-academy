dfc159600247db95eb7418a8d5d01f42f13bb3f2
Josep <josepcoronado1@gmail.com>
2021-09-01 18:36:41 +0200

    Finish practice

--- canvas_practice/practice.js
+++ canvas_practice/practice.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvasEl = document.getElementById('mycanvas');
+    canvasEl.width = 500;
+    canvasEl.height = 500;
 
+    const ctx = canvasEl.getContext('2d');
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 500, 500);
+
+    ctx.beginPath();
+    ctx.arc(250, 250, 30, 0, 2*Math.PI, true);
+    ctx.strokeStyle = 'orange';
+    ctx.lineWidth = 10;
+    ctx.stroke();
+
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
 });
