fbd3a3c739ed656b685059ac0e1911538afe805a
Josep <josepcoronado1@gmail.com>
2021-09-14 21:10:39 +0200

    Reducer

--- job-listing/frontend/store.js
+++ job-listing/frontend/store.js
@@ -3,12 +3,4 @@ import reducer from './reducer';
 
 const store = createStore(reducer);
 
-const selectLocation = (city, jobs) => ({
-    type: 'SWITCH_LOCATION',
-    city,
-    jobs
-});
-
-window.selectLocation = selectLocation;
-
 export default store;
