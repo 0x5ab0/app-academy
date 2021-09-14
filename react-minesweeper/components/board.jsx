a4cdd646e937163ac97e7e7eb5c4bf74c055d623
Josep <josepcoronado1@gmail.com>
2021-09-14 14:53:44 +0200

    Final tweaks

---
+++ react-minesweeper/components/board.jsx
@@ -0,0 +1,12 @@
+import Tile from './tile';
+import React from 'react';
+
+class Board extends React.Component{
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Board;
---
+++ react-minesweeper/components/game.jsx
@@ -0,0 +1,12 @@
+import Board from './board';
+import React from 'react';
+
+class Game extends React.Component{
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Game;
