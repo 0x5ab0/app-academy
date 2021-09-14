d3114e05d0c92a4a5ef774709936df7d7a9eb2ad
Josep <josepcoronado1@gmail.com>
2021-09-14 20:59:48 +0200

    Store and Action Creator

--- job-listing/frontend/store.js
+++ job-listing/frontend/store.js
@@ -1,1 +1,14 @@
+import { createStore } from 'redux';
+import reducer from './reducer';
 
+const store = createStore(reducer);
+
+const selectLocation = (city, jobs) => ({
+    type: 'SWITCH_LOCATION',
+    city,
+    jobs
+});
+
+window.selectLocation = selectLocation;
+
+export default store;
