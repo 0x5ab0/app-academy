2a72fb0560840295f6dfcb640733e84967777424
Josep <josepcoronado1@gmail.com>
2021-09-15 12:11:48 +0200

    Refactor

--- todos/frontend/actions/todo_actions.jsx
+++ todos/frontend/actions/todo_actions.jsx
@@ -1,18 +1,12 @@
 export const RECEIVE_TODOS = 'RECEIVE_TODOS';
 export const RECEIVE_TODO = 'RECEIVE_TODO';
 
-const receiveTodos = (todos) => {
-    return {
+export const receiveTodos = (todos) => ({
         type: RECEIVE_TODOS,
         todos
-    };
-};
+});
 
-const receiveTodo = (todo) => {
-    return {
-        type: RECEIVE_TODO,
-        todo
-    };
-};
-
-export default [receiveTodos, receiveTodo];
+export const receiveTodo = (todo) => ({
+    type: RECEIVE_TODO,
+    todo
+});
