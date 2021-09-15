3933db10865071d9e8d9b00ddf2bbbc60dadf1d2
Josep <josepcoronado1@gmail.com>
2021-09-15 13:41:29 +0200

    Add RECEIVE_TODOS and RECEIVE_TODO actions

--- todos/frontend/reducers/todos_reducer.js
+++ todos/frontend/reducers/todos_reducer.js
@@ -1,3 +1,9 @@
+import { RECEIVE_TODOS, RECEIVE_TODO, receiveTodo, receiveTodos } from '../actions/todo_actions';
+
+// Just for testing. Remove later.
+window.receiveTodo = receiveTodo;
+window.receiveTodos = receiveTodos;
+
 const initialState =  {
     1: {
         id: 1,
@@ -14,7 +20,18 @@ const initialState =  {
 };
 
 const todosReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    let nextState = {};
+
     switch (action.type) {
+        case RECEIVE_TODOS:
+            action.todos.forEach(todo => {
+                nextState[todo.id] = todo;
+            });
+            return nextState;
+        case RECEIVE_TODO:
+            const newTodo = { [action.todo.id]: action.todo };
+            return Object.assign({}, state, newTodo);
         default:
             return state;
     };
