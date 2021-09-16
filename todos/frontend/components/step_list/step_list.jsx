c9f6bdf8512352a0ed0466e4c06d1a18f2064417
Josep <josepcoronado1@gmail.com>
2021-09-16 23:27:08 +0200

    Step Form

--- todos/frontend/components/step_list/step_list.jsx
+++ todos/frontend/components/step_list/step_list.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import StepListItemContainer from './step_list_item_container'
-// import StepForm from './step_form'
+import StepForm from './step_form'
 
 const StepList = ({ steps, todo_id, receiveStep }) => {
     const stepItems = steps.map(step => (
@@ -14,7 +14,7 @@ const StepList = ({ steps, todo_id, receiveStep }) => {
             <ul>
                 { stepItems }
             </ul>
-            {/* <StepForm todo_id={todo_id} receiveStep={receiveStep}/> */}
+            <StepForm todo_id={todo_id} receiveStep={receiveStep}/>
         </div>
     );
 };
