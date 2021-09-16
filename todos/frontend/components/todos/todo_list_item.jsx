6537c6565ada8761b785166e9a3896e974f6f382
Josep <josepcoronado1@gmail.com>
2021-09-16 19:38:50 +0200

    Delete and Toggle

--- todos/frontend/components/todos/todo_list_item.jsx
+++ todos/frontend/components/todos/todo_list_item.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 class TodoListItem extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+    }
+
+    handleDelete(e) {
+        e.preventDefault();
+        const todo = this.props.todo;
+        this.props.removeTodo(todo);
+    }
+
+    handleUpdate(e) {
+        e.preventDefault();
+        const todo = this.props.todo;
+        this.toggle(todo);
+    }
+
+    toggle(todo) {
+        todo.done = todo.done ? false : true;
+        this.props.receiveTodo(todo);
     }
 
     render() {
@@ -13,7 +33,9 @@ class TodoListItem extends React.Component {
                 <div>
                     <h3>{ todo.title }</h3>
                 </div>
-            </li>    
+                <button onClick={this.handleDelete}>Delete</button>
+                <button onClick={this.handleUpdate}>{ todo.done ? 'Not done' : 'Done' }</button>
+            </li>
         );
     }
 };
