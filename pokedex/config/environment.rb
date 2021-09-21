20e34bad10eb9542c170a11042c1e72af8404eec
Josep <josepcoronado1@gmail.com>
2021-09-21 19:32:26 +0200

    Pokemon Redux Cycle

--- pokedex/config/environment.rb
+++ pokedex/config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Automatically camelCase state keys.
+Jbuilder.key_format camelize: :lower
--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+// // Testing
+// import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+// import { fetchAllPokemon } from './util/api_util';
+// import { selectAllPokemon } from './reducers/selectors';
+// window.receiveAllPokemon = receiveAllPokemon;
+// window.fetchAllPokemon = fetchAllPokemon;
+// window.requestAllPokemon = requestAllPokemon;
+// window.selectAllPokemon = selectAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+
+    // Testing
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
     const rootEl = document.getElementById('root');
     ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
